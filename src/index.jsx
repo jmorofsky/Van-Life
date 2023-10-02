ed10d483dfb7438dd391835d5b70dfbe3c22c826
unknown <jmorofsky@gmail.com>
2023-10-02 17:49:23 -0400

    added content to home and about pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import './index.css'
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-function Home() {
-  return (
-    <h1>Hello, React Router!</h1>
-  )
-}
-
-function About() {
-  return (
-    <h1>About page goes here! 🎉</h1>
-  )
+    return (
+        <BrowserRouter>
+            <header>
+                <Link to="/">#VANLIFE</Link>
+                <nav>
+                    <Link to="/about">About</Link>
+                </nav>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />)
+    .createRoot(document.getElementById('root'))
+    .render(<App />)
