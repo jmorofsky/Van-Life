ad8582facbd667185d3c2d6e11c43aa4e6860170
unknown <jmorofsky@gmail.com>
2023-10-02 17:32:27 -0400

    added about page

--- src/index.jsx
+++ src/index.jsx
@@ -8,10 +8,17 @@ function App() {
     )
 }
 
+function About() {
+    return (
+        <h1>About page goes here! 🎉</h1>
+    )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
+            <Route path='/about' element={<About />} />
         </Routes>
     </BrowserRouter>
 )
