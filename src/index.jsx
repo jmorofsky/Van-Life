6e47bd0c26c39073f80cf3fb56907bfdf241bdd1
unknown <jmorofsky@gmail.com>
2023-10-11 17:16:27 -0400

    moved navigation to separate file and nested route

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import './index.css'
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
+import "./index.css"
+
 import "./server"
-import Vans from './pages/Vans'
-import VanDetail from './pages/VanDetail'
 
 function App() {
-    return (
-        <BrowserRouter>
-            <header>
-                <Link to="/">#VANLIFE</Link>
-                <nav>
-                    <Link to="/about">About</Link>
-                    <Link to="/vans">Vans</Link>
-                </nav>
-            </header>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/vans" element={<Vans />} />
-                <Route path='/vans/:id' element={<VanDetail />} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 ReactDOM
-    .createRoot(document.getElementById('root'))
-    .render(<App />)
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
