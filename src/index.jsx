57956c696206eea34119b7f1ce673bed4e9868e3
unknown <jmorofsky@gmail.com>
2023-10-11 17:43:52 -0400

    added host navigation

--- src/index.jsx
+++ src/index.jsx
@@ -9,28 +9,32 @@ import Layout from "./components/Layout"
 import Dashboard from './pages/Host/Dashboard'
 import Income from './pages/Host/Income'
 import Reviews from './pages/Host/Reviews'
+import HostLayout from './components/HostLayout'
 import "./index.css"
 
 import "./server"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:id" element={<VanDetail />} />
+
+                    <Route path="/host" element={<HostLayout />} >
+                        <Route path="/host" element={<Dashboard />} />
+                        <Route path="/host/income" element={<Income />} />
+                        <Route path="/host/reviews" element={<Reviews />} />
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+    .createRoot(document.getElementById('root'))
+    .render(<App />)
